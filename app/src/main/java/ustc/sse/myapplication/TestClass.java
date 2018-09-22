f7856d84a66e3fbcea9783a167fd394745172e2c
eva-ustc <43311504+eva-ustc@users.noreply.github.com>
2018-09-22 10:42:08 +0800

    [init]初始化项目

---
+++ app/src/main/java/ustc/sse/myapplication/TestClass.java
@@ -0,0 +1,14 @@
+package ustc.sse.myapplication;
+
+import android.util.Log;
+
+/**
+ * Created by LRK on 2018/9/22.
+ */
+
+public class TestClass {
+
+    public TestClass(){
+        Log.d("TAG" , "Hello world!");
+    }
+}
